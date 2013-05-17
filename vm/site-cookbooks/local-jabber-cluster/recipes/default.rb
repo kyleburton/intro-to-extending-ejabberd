d7245c2870e14b44fd23b905e2b46b234be35d8f
Kyle Burton <kyle.burton@gmail.com>
2013-05-17 14:01:15 -0400

    vm configs

--- vm/site-cookbooks/local-jabber-cluster/recipes/default.rb
+++ vm/site-cookbooks/local-jabber-cluster/recipes/default.rb
@@ -12,11 +12,19 @@ end
 nodenum = $1.to_i + 1
 ejabberd_hostname = "ejabberd0#{nodenum}"
 
+# NB: this must match what is in the Vagrantfile
+jabber_hosts = {
+  'ejabberd01' => ["10.1.0.11 ejabberd02"],
+  'ejabberd02' => ["10.1.0.10 ejabberd01"]
+}
+
+
 template "/etc/hosts" do
   source "hosts.erb"
   mode 644
   variables(
-    :hostname => ejabberd_hostname
+    :hostname                 => ejabberd_hostname,
+    :other_jabber_hostentries => jabber_hosts[ejabberd_hostname]
   )
 end
 
